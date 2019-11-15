c50d6181ee2a4da39bbb592faaa3436000fdd23f
alumno <alumno@pc05.iris.majuelo.es>
2019-11-15 19:04:06 +0100

    Añadido la vista menu

--- src/Vistas/Menu.java
+++ src/Vistas/Menu.java
@@ -5,17 +5,51 @@
  */
 package Vistas;
 
+import javax.swing.JDialog;
+
 /**
  *
  * @author alumno
  */
 public class Menu extends javax.swing.JFrame {
-
+    InicioSesion in;
+    VerB b;
+    VerC c;
     /**
      * Creates new form Menu
      */
     public Menu() {
         initComponents();
+        jMenuBar1.setVisible(false);
+        in = new InicioSesion();
+        this.setContentPane(in);
+        this.pack();
+    }
+    
+    public void setBC(VerB bb,VerC cc){
+        b=bb;
+        c=cc;
+    }
+    
+    public void verB(){
+        jMenuBar1.setVisible(true);
+        this.setContentPane(b);
+        this.pack();
+    }
+    
+    public void verC(){
+        this.setContentPane(c);
+        this.pack();
+    }
+    
+    public void insertarB(){
+        this.setContentPane(new InsertarB());
+        this.pack();
+    }
+    
+    public void incertarC(){
+        this.setContentPane(new InsertarC());
+        this.pack();
     }
 
     /**
@@ -27,7 +61,34 @@ public class Menu extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
+        jMenuBar1 = new javax.swing.JMenuBar();
+        jMenu1 = new javax.swing.JMenu();
+        InicioSesionMenuItem = new javax.swing.JMenuItem();
+        AcercaDeMenuItem = new javax.swing.JMenuItem();
+
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+
+        jMenu1.setText("Menu");
+
+        InicioSesionMenuItem.setText("Cambiar Usuario");
+        InicioSesionMenuItem.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                InicioSesionMenuItemActionPerformed(evt);
+            }
+        });
+        jMenu1.add(InicioSesionMenuItem);
+
+        AcercaDeMenuItem.setText("AcercaDe");
+        AcercaDeMenuItem.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                AcercaDeMenuItemActionPerformed(evt);
+            }
+        });
+        jMenu1.add(AcercaDeMenuItem);
+
+        jMenuBar1.add(jMenu1);
+
+        setJMenuBar(jMenuBar1);
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
@@ -37,11 +98,23 @@ public class Menu extends javax.swing.JFrame {
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 300, Short.MAX_VALUE)
+            .addGap(0, 279, Short.MAX_VALUE)
         );
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
+
+    private void InicioSesionMenuItemActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_InicioSesionMenuItemActionPerformed
+        // TODO add your handling code here:
+        this.setContentPane(in);
+        this.pack();
+    }//GEN-LAST:event_InicioSesionMenuItemActionPerformed
+
+    private void AcercaDeMenuItemActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_AcercaDeMenuItemActionPerformed
+        // TODO add your handling code here:
+        JDialog j= new AcercaDe(this, true);
+        j.setVisible(true);
+    }//GEN-LAST:event_AcercaDeMenuItemActionPerformed
 
     /**
      * @param args the command line arguments
@@ -79,5 +152,9 @@ public class Menu extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JMenuItem AcercaDeMenuItem;
+    private javax.swing.JMenuItem InicioSesionMenuItem;
+    private javax.swing.JMenu jMenu1;
+    private javax.swing.JMenuBar jMenuBar1;
     // End of variables declaration//GEN-END:variables
 }
