718e6a192fc6cfbae0c6eb3a4a534eebe265be84
VladVolM <vladvolm@gmail.com>
2019-11-22 11:56:04 +0100

    para los ficheros

--- src/Controlador/ControlDeFichero.java
+++ src/Controlador/ControlDeFichero.java
@@ -11,4 +11,49 @@ package Controlador;
  */
 public class ControlDeFichero {
     
+    
+    /*
+    
+    // Java Program to illustrate reading from FileReader 
+// using BufferedReader 
+import java.io.*; 
+public class ReadFromFile2 
+{ 
+  public static void main(String[] args)throws Exception 
+  { 
+  // We need to provide file path as the parameter: 
+  // double backquote is to avoid compiler interpret words 
+  // like \test as \t (ie. as a escape sequence) 
+  File file = new File("C:\\Users\\pankaj\\Desktop\\test.txt"); 
+  
+  BufferedReader br = new BufferedReader(new FileReader(file)); 
+  
+  String st; 
+  while ((st = br.readLine()) != null) 
+    System.out.println(st); 
+  } 
+}
+    
+    
+    public static void usingBufferedWritter() throws IOException 
+{
+    String fileContent = "Hello Learner!";
+     
+    BufferedWriter writer = new BufferedWriter(new FileWriter("c:/temp/samplefile1.txt"));
+    writer.write(fileContent);
+    writer.close();
+}
+
+    
+    try (FileOutputStream fos = new FileOutputStream("pathname")) {
+   fos.write(myByteArray);
+   //fos.close(); There is no more need for this line since you had created the instance of "fos" inside the try. And this will automatically close the OutputStream
+}
+    
+    File file;
+// ...(file is initialised)...
+byte[] fileContent = Files.readAllBytes(file.toPath());
+    
+    
+    */
 }
